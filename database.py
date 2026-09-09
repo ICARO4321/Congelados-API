bf4cd1ba924eafd9ebe6be5c9ae24d09faf64181
Ícaro <icaroandradeho@gmail.com>
2026-09-09 18:49:54 -0300

    Usa banco Postgres do Neon direto no codigo

--- database.py
+++ database.py
@@ -2,13 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Configuração do SQLite (cria um arquivo local chamado 'congelados.db' para salvar os dados)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./congelados.db"
+# URL direta do Neon que você gerou
+DATABASE_URL = "postgresql://neondb_owner:npg_CMzTfJI8mji1@ep-aged-sun-ae6g3m54-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
